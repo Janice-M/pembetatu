4ef765a0db41d41c38181f11c2444efc271b0422
Janice-M <janiceink001@gmail.com>
2019-06-03 11:21:42 +0300

    final indentation

--- script.js
+++ script.js
@@ -1,27 +1,23 @@
-function triangleTracker(first,second,third) {
+function triangleTracker(first, second, third) {
 
   var first = document.getElementById('firstSide').value;
   var second = document.getElementById('secondSide').value;
   var third = document.getElementById('thirdSide').value;
 
-  if(first === second && second === third){
+  if (first === second && second === third) {
     if (first == "" || first.length == 0 || first == null && second == "" || second.length == 0 || second == null && third == "" || third.length == 0 || third == null) {
       alert("Not a triangle buddy!");
     } else {
       alert("This is an equilateral triangle");
     }
-  }
-
-  else if (first === second || second === third || first  === third) {
-    if (first == "" || first.length == 0 ||first == 0 || first == null ||second==0|| second == "" || second.length == 0 || second == null ||third == 0|| third == "" || third.length == 0 || third == null) {
+  } else if (first === second || second === third || first === third) {
+    if (first == "" || first.length == 0 || first == 0 || first == null || second == 0 || second == "" || second.length == 0 || second == null || third == 0 || third == "" || third.length == 0 || third == null) {
       alert("Not a triangle buddy!");
     } else {
-        alert("This is an isosceles triangle");
-      }
+      alert("This is an isosceles triangle");
     }
-
-  else {
-    alert ("This is a scalene triangle");
+  } else {
+    alert("This is a scalene triangle");
   }
 
 }
